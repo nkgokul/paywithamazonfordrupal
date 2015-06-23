38508ad592e20aad31537fd35a3de7606c6e1e00
Gokul N K <nkgokul@gmail.com>
2015-06-24 03:50:43 +0530

    first commit

---
+++ README.txt
@@ -0,0 +1,46 @@
+CHECKOUT BY AMAZON SIGNATURE GENERATION SAMPLE CODE
+Author: Nitin Purswani
+Copyright: 2007-2014 Amazon Technologies, Inc
+*-*-**-***-*****-********-*************
+ 
+*-*-**-***-*****-********-*************
+CONTENT SECTIONS (in order of appearance)
+*-*-**-***-*****-********-*************
+	INTRODUCTION
+	PREREQUISITES
+	RELEASE NOTES
+	SUPPORT & PROJECT HOME
+	LINKS
+*-*-**-***-*****-********-*************
+INTRODUCTION
+*-*-**-***-*****-********-*************
+	Please understand that by installing Checkout by Amazon signing sample
+        code, you are agreeing to understand and abide by the terms of the license, as written in LICENSE.txt.  
+	Important links are grouped together in a separate section for your convenience.  The most current documentation on Checkout by Amazon can be found on its website.  
+
+*-*-**-***-*****-********-*************
+PREREQUISITES
+*-*-**-***-*****-********-*************
+Please have the following software packages available on your systems
+before running the demo.
+
+1. PHP 5
+
+
+*-*-**-***-*****-********-*************
+RELEASE NOTES 
+*-*-**-***-*****-********-*************
+(1) Carefully follow all instructions in INSTALLATION_GUIDE.txt 
+(2) You must have set up an Amazon Seller account & have your merchantID
+(3) You must have set up an AWS account, and have your AWS keys ready to use in
+the demo. To read more about an AWS account, please view Seller Central help: https://sellercentral-europe.amazon.com/gp/help/help-home.html
+
+*-*-**-***-*****-********-*************
+SUPPORT & PROJECT HOME
+*-*-**-***-*****-********-*************
+	The latest documentation on Checkout by Amazon can be found at in the LINKS section below.
+*-*-**-***-*****-********-*************
+LINKS
+*-*-**-***-*****-********-*************
+	Checkout by Amazon Documentation & Seller Central
+		https://sellercentral-europe.amazon.com/gp/help/help-home.html 
---
+++ signature/common/cart/xml/XMLCartFactory.php
@@ -0,0 +1,109 @@
+<?php
+require_once(libraries_get_path('pay_with_amazon') . "/signature/common/cart/CartFactory.php");
+                                                                                                                                                            /**
+ * Returns a simple static cart to generate a signature from,
+ * and the final complete cart html.
+ *
+ * Copyright 2008-2011 Amazon.com, Inc., or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *    http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file.
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+class XMLCartFactory extends CartFactory {
+   protected static $CART_ORDER_INPUT_FIELD ="type:merchant-signed-order/aws-accesskey/1;order:[ORDER];signature:[SIGNATURE];aws-access-key-id:[AWS_ACCESS_KEY_ID]";
+	
+   public function XMLCartFactory() {
+   }
+
+   /**
+    * Gets cart html fragment used to generate entire cart html
+    * Base 64 encode the cart.
+    * 
+    */
+    public function getCart($merchantID, $awsAccessKeyID) {
+        $cartXML = $this->getCartXML($merchantID, $awsAccessKeyID);
+        return base64_encode($cartXML);
+    }
+   
+   /**
+    * Returns the concatenated cart used for signature generation.
+    * @see CartFactory
+    */
+   public function getSignatureInput($merchantID, $awsAccessKeyID) {
+        return $this->getCartXML($merchantID, $awsAccessKeyID);
+   }
+
+   /**
+    * Returns a finalized full cart html including the base 64 encoded cart,
+    * signature, and buy button image link.
+    */
+   public function getCartHTML($merchantID, $awsAccessKeyID, $signature) {
+        $cartHTML = '';
+
+	$cartHTML = $cartHTML . CartFactory::$CART_JAVASCRIPT_START;
+	$cartHTML = $cartHTML . CartFactory::$CBA_BUTTON_DIV;       
+	// construct the order-input section
+	$encodedCart = $this->getCart($merchantID, $awsAccessKeyID);
+        $input = ereg_replace("\\[ORDER\\]", $encodedCart, XMLCartFactory::$CART_ORDER_INPUT_FIELD);
+        $input = ereg_replace("\\[SIGNATURE\\]", $signature, $input);
+        $input = ereg_replace("\\[AWS_ACCESS_KEY_ID\\]", $awsAccessKeyID, $input);
+        $widgetScript = ereg_replace("\\[CART_TYPE\\]", "XML",CartFactory::$STANDARD_CHECKOUT_WIDGET_SCRIPT);
+        $widgetScript = ereg_replace("\\[MERCHANT_ID\\]", $merchantID,$widgetScript);
+        $widgetScript =ereg_replace ("\\[CART_VALUE\\]",$input ,$widgetScript);
+
+        $cartHTML = $cartHTML . $widgetScript;        
+
+	return $cartHTML;
+   }
+	
+    /**
+     * Replace with your own cart here to try out
+     * different promotions, tax, shipping, etc. 
+     * 
+     * @param merchantID
+     * @param awsAccessKeyID
+     */
+    private function getCartXML($merchantID, $awsAccessKeyID) {
+      $xml = 
+	"<?xml version=\"1.0\" encoding=\"UTF-8\"?>" .
+	"<Order xmlns=\"http://payments.amazon.com/checkout/2008-06-15/\">" .
+	"    <ClientRequestId>123457</ClientRequestId>" .
+	"    <Cart>" .
+	"    <Items>";
+      global $user;
+      $cart = commerce_cart_order_load($user->uid);
+      foreach ($cart->commerce_line_items['und'] as $line_item_id) {
+	$line_item = commerce_line_item_load($line_item_id);
+	list($quantity) = explode('.', $line_item->quantity);
+	  $xml .=  "      <Item>" .
+	    "         <SKU>" . $line_item_id->line_item_label . "</SKU>" .
+	    "         <MerchantId>" . $merchantID . "</MerchantId>" .
+	    "         <Title>" . $line_item->line_item_label . "</Title>" .
+	    "         <Description>" . $line_item->line_item_label . "</Description>" .
+	    "         <Price>" .
+	    "            <Amount>" . (0 + $line_item->commerce_unit_price['und'][0]['amount'] / 100) . "</Amount>" .
+	    "            <CurrencyCode>INR</CurrencyCode>" .
+	    "         </Price>" .
+	    "         <Quantity> " . $quantity . "</Quantity>" .
+	    "         <Weight>" .
+	    "            <Amount>.5</Amount>" .
+	    "            <Unit>kg</Unit>" .
+	    "         </Weight>" .
+	    "         <Category>Tshirts</Category>" .
+	    "      </Item>" ;
+
+      }
+      $xml .= 
+	"    </Items>" .
+	"    </Cart>" .
+	"</Order>";
+	return $xml;
+    }
+}
